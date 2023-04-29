26d09fa713a50a80d8fa6f43e3a2fae10f5dc740
Crivetkaaa <crivetkaa@vk.com>
2023-04-29 23:52:05 +0300

    Отформатированно по стандартам языка

--- utils/database.py
+++ utils/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class DB:
     @classmethod
     def execute_res(cls, text):
@@ -11,32 +12,33 @@ class DB:
 
         cls.close_conn(conn, cur)
         return data
-    
+
     @classmethod
     def execute(cls, text):
         conn, cur = cls.open_conn()
         cur.execute(text)
         cls.close_conn(conn, cur)
-    
+
     @staticmethod
     def close_conn(conn, cur):
         conn.commit()
         cur.close()
         conn.close()
-    
+
     @staticmethod
     def open_conn():
         conn = sqlite3.connect('bot_db.sqlite')
         cur = conn.cursor()
         return conn, cur
-    
+
     @classmethod
     def input_file(cls, data):
         for row in data:
-            cls.execute(f"""INSERT INTO input_invoice (type_purchase, supplier, postponement, product_name, count, price, from_country, date, number) VALUES ('{row[0]}','{row[1]}','{row[2]}','{row[3]}','{row[4]}','{row[5]}','{row[6]}','{row[7]}','{row[8]}');""")
+            cls.execute(
+                f"""INSERT INTO input_invoice (type_purchase, supplier, postponement, product_name, count, price, from_country, date, number) VALUES ('{row[0]}','{row[1]}','{row[2]}','{row[3]}','{row[4]}','{row[5]}','{row[6]}','{row[7]}','{row[8]}');""")
 
     @classmethod
     def output_file(cls, data):
         for row in data:
-            cls.execute(f"INSERT INTO send_product (buyer, product_name, number, count, price, date, receipt) VALUES ('{row[0]}','{row[1]}','{row[2]}','{row[3]}','{row[4]}','{row[5]}','{row[6]}');")
-    
+            cls.execute(
+                f"INSERT INTO send_product (buyer, product_name, number, count, price, date, receipt) VALUES ('{row[0]}','{row[1]}','{row[2]}','{row[3]}','{row[4]}','{row[5]}','{row[6]}');")
