3b8e8046a16d5814f9d7bb6c34d875155ea6ffc9
Crivetkaaa <crivetkaa@vk.com>
2023-04-29 23:46:42 +0300

    Добавлены столбцы даты и чека

--- create_db.py
+++ create_db.py
@@ -2,23 +2,26 @@ from utils.database import DB
 
 def create_table():
     DB.execute("""CREATE TABLE IF NOT EXISTS input_invoice (
-        type_purchase TEXT,
-        supplier TEXT,
-        postponement TEXT,
-        product_name TEXT,
-        count INT,
-        price INT,
-        from_country TEXT,
-        number TEXT
+    type_purchase TEXT,
+    supplier      TEXT,
+    postponement  TEXT,
+    product_name  TEXT,
+    count         INT,
+    price         INT,
+    from_country  TEXT,
+    date          TEXT,
+    number        TEXT UNIQUE ON CONFLICT IGNORE
     );""")
 
     DB.execute("""CREATE TABLE IF NOT EXISTS send_product (
-    buyer TEXT,
+    buyer        TEXT,
     product_name TEXT,
-    number TEXT,
-    count INT,
-    price INT
-    );""")
+    number       TEXT UNIQUE ON CONFLICT IGNORE,
+    count        INT,
+    price        INT,
+    date         TEXT,
+    receipt INT
+);""")
     print('Таблицы успешно созданы!')
 
 if __name__ == '__main__':
